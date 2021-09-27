590bc71166d1680108bcd69d8e20e5a350aeee62
ShigureAya <shigureaya0920@gmail.com>
2021-09-27 21:07:30 +0900

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const modName = require('./package.json').modName;
+
+const CopyPlugin = require("copy-webpack-plugin");
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+module.exports = {
+    mode: 'production',
+    devtool: 'inline-source-map',
+    entry: path.join(__dirname, 'src/main.js'),
+    output: {
+        filename: 'main.js',
+        path: path.join(__dirname, 'dist', modName)
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new CopyPlugin({
+            patterns: [
+                {from: path.join(__dirname, 'static'), to: path.join(__dirname, 'dist', modName)},
+            ]
+        })
+    ],
+};
